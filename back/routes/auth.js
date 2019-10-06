5afe9a5a29303f8dff40f59c6a7772af00081e77
“Natalia <natgarea@gmail.com>
2019-10-06 21:37:09 +0200

    auth routes

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -2,50 +2,74 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+// const Organization = require("../models/Organization");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+const login = (req, user) => {
+  return new Promise((resolve,reject) => {
+    req.login(user, err => {
+      console.log('req.login ')
+      console.log(user)
 
-router.post("/signup", (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
-    return;
+      
+      if(err) {
+        reject(new Error('Something went wrong'))
+      }else{
+        resolve(user);
+      }
+    })
+  })
+}
+
+
+// SIGNUP
+router.post('/signup', (req, res, next) => {
+
+  const {username, password} = req.body;
+
+  console.log('username', username)
+  console.log('password', password)
+
+  // Check for non empty user or password
+  if (!username || !password){
+    next(new Error('You must provide valid credentials'));
   }
 
-  User.findOne({ username }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
-      return;
-    }
+  // Check if user exists in DB
+  User.findOne({ username })
+  .then( foundUser => {
+    if (foundUser) throw new Error('Username already exists');
 
-    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const salt     = bcrypt.genSaltSync(10);
     const hashPass = bcrypt.hashSync(password, salt);
 
-    const newUser = new User({
+    return new User({
       username,
       password: hashPass
-    });
-
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
-  });
+    }).save();
+  })
+  .then( savedUser => login(req, savedUser)) // Login the user using passport
+  .then( user => res.json({status: 'signup & login successfully', user})) // Answer JSON
+  .catch(e => next(e));
 });
+
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, theUser, failureDetails) => {
+    
+    // Check for errors
+    if (err) next(new Error('Something went wrong')); 
+    if (!theUser) next(failureDetails)
+
+    // Return user and logged in
+    login(req, theUser).then(user => res.status(200).json(req.user));
+
+  })(req, res, next);
+});
+
 
 router.get('/currentuser', (req,res,next) => {
   if(req.user){
