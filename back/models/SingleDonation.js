65fc7210579ec773a7cfa88650c8456db0cb836a
“Natalia <natgarea@gmail.com>
2019-10-15 12:08:47 +0200

    Updates seed to display some donations

--- back/models/SingleDonation.js
+++ back/models/SingleDonation.js
@@ -3,6 +3,8 @@ const Schema   = mongoose.Schema;
 
 const singleDonationSchema = new Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  org: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  campaign: { type: mongoose.Schema.Types.ObjectId, ref: 'Campaign' },
   anonymousDonation: Boolean,
   amountDonated: Number,
   comment: String,
