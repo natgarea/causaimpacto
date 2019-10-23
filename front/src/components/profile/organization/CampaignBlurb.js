1ffd9cddc268ecad95fd0b514bb57788a45f231f
“Natalia <natgarea@gmail.com>
2019-10-23 23:03:30 +0200

    fix campaigns link

--- front/src/components/profile/organization/CampaignBlurb.js
+++ front/src/components/profile/organization/CampaignBlurb.js
@@ -4,22 +4,28 @@ import { Link } from "react-router-dom";
 export default class CampaignBlurb extends Component {
   render() {
     return (
-   <div className="card column is-3">
-  <div className="card-content blurb">
-    <div className="content">
-    <figure className="image">
-      <img className="blurb-image" src={this.props.image} alt={this.props.title} />
-    </figure>
-    </div>
-    <div className="content blurb-text-size">
-    
-    <p className="title is-4">{this.props.title}</p>
-    <Link to={`/campaign/${this.props.id}`} className="button is-primary is-medium is-fullwidth final">Ver campaña</Link>
-    </div>
-    </div>
-    
-
-  </div>
-  );
+      <div className="card column is-3">
+        <div className="card-content blurb">
+          <div className="content">
+            <figure className="image">
+              <img
+                className="blurb-image"
+                src={this.props.image}
+                alt={this.props.title}
+              />
+            </figure>
+          </div>
+          <div className="content blurb-text-size">
+            <p className="title is-4">{this.props.title}</p>
+          </div>
+          <Link
+              to={`/campaign/${this.props.id}`}
+              className="button is-primary is-fullwidth final"
+            >
+              Ver campaña
+            </Link>
+        </div>
+      </div>
+    );
   }
 }
