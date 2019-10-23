89f25f4b28fba0cb21832ab5525226138243da61
“Natalia <natgarea@gmail.com>
2019-10-23 22:24:09 +0200

    main site background

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -48,56 +48,81 @@ export default class Main extends Component {
     console.log(this.state.results);
     return (
       <React.Fragment>
-        <div className="align-items-center">
-        <div className="align-items-center main-text">
-        <img src="./images/logo.png" alt="logo" />
-        <p className="has-margin-3"><h3 className="title is-3">¿Alguna vez has querido ayudar a otros pero no sabías cómo?</h3> <span className="txt-is-orange is-bold">causa</span>
-          <span className="txt-is-blue is-bold">impacto</span> pretende conectar a personas como tú con organizaciones que ayudan a individuos en situaciones difíciles o de emergencia.</p>
-          <p>Comienza a buscar organizaciones y campañas de tu interés:</p>
-        </div>
-        <div className="main-search">
-          <div className="field has-addons">
-            <div className="control">
-              <div className="dropdown is-active">
-                <div className="dropdown-trigger">
-                  <input
-                    className="input search-input"
-                    type="text"
-                    value={this.state.searchTerm}
-                    placeholder="Busca una organización o campaña"
-                    name="searchTerm"
-                    onChange={e => this.handleChange(e)}
-                  />
-                </div>
-                {this.state.results.length > 0 && (
-                  <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                    <div className="dropdown-content d-main">
-                      {this.state.results.map((result, i) => (
-                        <Link to={result.uri} className="dropdown-item">
-                          {result.title}
-                        </Link>
-                      ))}
+        <div className="main">
+          <div className="align-items-center">
+            <div className="align-items-center main-text">
+              <img src="./images/logo.png" alt="logo" />
+              <p className="has-margin-3">
+                <h3 className="title is-3">
+                  ¿Alguna vez has querido ayudar a otros pero no sabías cómo?
+                </h3>{" "}
+                <span className="txt-is-orange is-bold">causa</span>
+                <span className="txt-is-blue is-bold">impacto</span> pretende
+                conectar a personas como tú con organizaciones que ayudan a
+                individuos en situaciones difíciles o de emergencia.
+              </p>
+              <p>Comienza a buscar organizaciones y campañas de tu interés:</p>
+            </div>
+            <div className="main-search">
+              <div className="field has-addons">
+                <div className="control">
+                  <div className="dropdown is-active">
+                    <div className="dropdown-trigger">
+                      <input
+                        className="input search-input"
+                        type="text"
+                        value={this.state.searchTerm}
+                        placeholder="Busca una organización o campaña"
+                        name="searchTerm"
+                        onChange={e => this.handleChange(e)}
+                      />
                     </div>
+                    {this.state.results.length > 0 && (
+                      <div
+                        className="dropdown-menu"
+                        id="dropdown-menu"
+                        role="menu"
+                      >
+                        <div className="dropdown-content d-main">
+                          {this.state.results.map((result, i) => (
+                            <Link to={result.uri} className="dropdown-item">
+                              {result.title}
+                            </Link>
+                          ))}
+                        </div>
+                      </div>
+                    )}
                   </div>
-                )}
+                </div>
+                <div className="control">
+                  <Link to="#" className="button is-info">
+                    <span className="icon is-medium">
+                      <FontAwesomeIcon icon={faSearch} aria-hidden="true" />
+                    </span>
+                  </Link>
+                </div>
               </div>
             </div>
-            <div className="control">
-              <Link to="#" className="button is-info">
-                <span className="icon is-medium">
-                  <FontAwesomeIcon icon={faSearch} aria-hidden="true" />
-                </span>
-              </Link>
+          </div>
+          <div>
+            <div className="main-category has-margin-4 categories">
+              <h3 className="title is-4">Busca por categoría</h3>
+              <div className="categories">
+                {this.props.categoryList.map((category, i) => (
+                  <div className="category-main column is-one-fifth" key={i}>
+                    <Link to={`/category/${category._id}`}>
+                      <img
+                        src={category.image}
+                        className="category-img has-margin-1"
+                        alt={category.name}
+                      />{" "}
+                      <h5 className="title is-6">{category.name}</h5>
+                    </Link>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="main-category has-margin-4 categories">
-          <h3 className="title is-4">Busca por categoría</h3>
-        <div className="categories">
-          {this.props.categoryList.map((category,i) => 
-          (<div className="category-main column is-one-fifth" key={i}><Link to={`/category/${category._id}`}><img src={category.image} className="category-img has-margin-1" alt={category.name}/> <h5 className="title is-6">{category.name}</h5></Link></div>))}
-        </div></div>
         </div>
       </React.Fragment>
     );
